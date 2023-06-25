f760ba5fa8de189a4a0c2a64d92278ea450fefbc
Mustafa Çağrı Güven <mustafacagri@users.noreply.github.com>
2023-06-25 16:12:37 +0300

    fix: use navigate to open a link from the customHeader

--- src/components/elements/CustomHeader.js
+++ src/components/elements/CustomHeader.js
@@ -1,9 +1,10 @@
 import Box from '@mui/material/Box'
 import BottomNavigation from '@mui/material/BottomNavigation'
 import BottomNavigationAction from '@mui/material/BottomNavigationAction'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 function CustomHeader() {
+  const navigate = useNavigate()
   const menuItems = [
     { icon: 'Topic', to: '/', label: 'All Todos' },
     { icon: 'Info', to: '/about', label: 'About' }
@@ -13,13 +14,7 @@ function CustomHeader() {
     <Box>
       <BottomNavigation showLabels sx={{ backgroundColor: '#E7EBF0' }}>
         {menuItems.map(item => (
-          <BottomNavigationAction key={item.to}
-            label={
-              <Link to={item.to} >
-                {item.label}
-              </Link>
-            }
-          />
+          <BottomNavigationAction key={item.to} onClick={() => navigate(item.to)} label={item.label} />
         ))}
       </BottomNavigation>
     </Box>
